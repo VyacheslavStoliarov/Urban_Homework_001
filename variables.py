ff482b91dc8965729c539cd3083151efe4e4196c
bit <slawastoliarow@yandex.ru>
2024-12-19 14:35:49 +0700

    Переменные

---
+++ variables.py
@@ -0,0 +1,5 @@
+count_hw = 12
+count_hours = 1.5
+name_course = 'Python'
+time_task = count_hours/count_hw
+print('Курс:', name_course, ', всего задач:', count_hw, ', затрачено часов:', count_hours, ', среднее время выполнения:', time_task, 'часа.')
