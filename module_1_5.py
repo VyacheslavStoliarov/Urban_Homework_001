74d0dee436fff56ec08b2bfff6d9f22a64c6b3fa
bit <slawastoliarow@yandex.ru>
2024-12-21 00:41:25 +0700

    Неизменяемые и изменяемые объекты. Кортежи

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 4, 'alfa', 5.8, True, ['a', '0']
+print(immutable_var)
+# immutable_var[3] = 4 -- 'tuple' object does not support item assignment
+mutable_list = [141, 637, 2, 445]
+mutable_list[2] = -2
+print(mutable_list)
